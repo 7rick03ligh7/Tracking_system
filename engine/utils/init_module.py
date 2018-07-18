f958b344f40ac0359fa2f4d9cfcbdada4f9ebf33
edefanov <efanov.zhenya@gmail.com>
2018-07-18 15:24:02 +0300

    Init module as a class

--- engine/utils/init_module.py
+++ engine/utils/init_module.py
@@ -1,32 +1,36 @@
-# обязательно
-# всунуть куда-нибудь в начало
+#обязательно
+#всунуть куда-нибудь в начало
 from onvif import ONVIFCamera
 from time import sleep
 import math
 
+class InitModule:
+    def __init__():
+        #здесь где-то парсинг конфига
 
-def initModule():
 
-    # здесь где-то парсинг конфига
+        #айпи-логин-пасс захардкодил пока нет инфы про работу веб штук
+        ip = '192.167.11.43'
+        port = 80
+        login = 'admin'
+        password = 'Supervisor'
+        
+        mycam = ONVIFCamera(ip, port, login, password) 
 
-    # айпи-логин-пасс захардкодил пока нет инфы про работу веб штук
-    mycam = ONVIFCamera('192.168.11.43', 80, 'admin', 'Supervisor')
+        media = mycam.create_media_service()
+        profile = media.GetProfiles()[0]
+        token = profile._token
+        
+        ptz = mycam.create_ptz_service()
+        request = ptz.create_type('GetConfigurationOptions')
+        request.ConfigurationToken = profile.PTZConfiguration._token
+        ptz_configuration_options = ptz.GetConfigurationOptions(request)
 
-    media = mycam.create_media_service()
-    profile = media.GetProfiles()[0]
-    token = profile._token
-
-    ptz = mycam.create_ptz_service()
-    request = ptz.create_type('GetConfigurationOptions')
-    request.ConfigurationToken = profile.PTZConfiguration._token
-    ptz_configuration_options = ptz.GetConfigurationOptions(request)
-
-    request = ptz.create_type('ContinuousMove')
-    request.ProfileToken = profile._token
-
-    # получает юрл в структуре
-    streamURIStruct = media.GetStreamUri(
-        {'StreamSetup': {'Stream': 'RTP-Unicast', 'Transport': 'UDP'}, 'ProfileToken': token})
-
-    # непосредственно сам ЮРЛ потока в переменной для всовывания в опенСВ
-    streamURL = streamURIStruct.Uri
+        request = ptz.create_type('ContinuousMove')
+        request.ProfileToken = profile._token
+    
+        #получает юрл в структуре
+        streamURIStruct = media.GetStreamUri({'StreamSetup':{'Stream':'RTP-Unicast','Transport':'UDP'},'ProfileToken':token}) 
+    
+        #непосредственно сам ЮРЛ потока в переменной для всовывания в опенСВ
+        streamURL = streamURIStruct.Uri
