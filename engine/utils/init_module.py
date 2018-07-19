cdd915412683aaa124215d84771f06b0c02a4b62
edefanov <efanov.zhenya@gmail.com>
2018-07-19 03:18:48 +0300

    Move module

--- engine/utils/init_module.py
+++ engine/utils/init_module.py
@@ -1,36 +1,32 @@
-#обязательно
-#всунуть куда-нибудь в начало
+# mandatory
 from onvif import ONVIFCamera
 from time import sleep
 import math
+import zeep
 
 class InitModule:
-    def __init__():
-        #здесь где-то парсинг конфига
-
-
-        #айпи-логин-пасс захардкодил пока нет инфы про работу веб штук
-        ip = '192.167.11.43'
+    def zeep_pythonvalue(self, xmlvalue):
+        return xmlvalue
+    def __init__(self):
+        # @todo: config parsing
+        zeep.xsd.simple.AnySimpleType.pythonvalue = zeep_pythonvalue
+        # hardcoded ip-port-log-pass
+        ip = '192.168.11.43'
         port = 80
         login = 'admin'
         password = 'Supervisor'
-        
         mycam = ONVIFCamera(ip, port, login, password) 
-
         media = mycam.create_media_service()
-        profile = media.GetProfiles()[0]
-        token = profile._token
-        
+        self.profile = media.GetProfiles()[0]
+        token = profile.token
         ptz = mycam.create_ptz_service()
         request = ptz.create_type('GetConfigurationOptions')
-        request.ConfigurationToken = profile.PTZConfiguration._token
+        request.ConfigurationToken = profile.PTZConfiguration.token
         ptz_configuration_options = ptz.GetConfigurationOptions(request)
-
         request = ptz.create_type('ContinuousMove')
-        request.ProfileToken = profile._token
-    
-        #получает юрл в структуре
+        request.ProfileToken = profile.token
+        self.request2 = request
+        # gets url in a struct
         streamURIStruct = media.GetStreamUri({'StreamSetup':{'Stream':'RTP-Unicast','Transport':'UDP'},'ProfileToken':token}) 
-    
-        #непосредственно сам ЮРЛ потока в переменной для всовывания в опенСВ
-        streamURL = streamURIStruct.Uri
+        # the url itself
+        self.streamURL = streamURIStruct.Uri      
