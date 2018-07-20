3efc4277ac97efbed670dfdb74130fe23f83344d
DiggiDon <aleksey230997@gmail.com>
2018-07-20 11:19:51 +0300

    Add: append sys.path in track_test.py

--- track_test.py
+++ track_test.py
@@ -1,5 +1,8 @@
 from engine.track_system import Tracking_system
+import os
+import sys
 
 if __name__ == '__main__':
+    sys.path.append(os.getcwd())
     track = Tracking_system()
     track.run()
